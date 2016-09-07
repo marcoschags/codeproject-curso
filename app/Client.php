7d0df10e2852a9651bce21b2d59a7572f56d61be
marcoschags <marcoschags@hotmail.com>
2016-09-07 19:01:32 +0000

    Factory Client

--- app/Client.php
+++ app/Client.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
- 	protected $fillable  [ 
- 		'name',
- 		'responsible',
- 		'email',
- 		'phone',
- 		'address',
- 		'obs'
- 	];
+ 	protected $fillable = [
+ 	    'name',
+        'responsible',
+        'email',
+        'phone',
+        'address',
+        'obs'
+    ];
 }
