d8d7f846753442dcae38f205f4ce9c303c176ae8
marcoschags <marcoschags@hotmail.com>
2016-09-14 02:14:36 +0000

    Criando primeiro Repository

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,7 +2,8 @@
 
 namespace CodeProject\Http\Controllers;
 
-use CodeProject\Models\Client;
+use CodeProject\Entities\Client;
+use CodeProject\Repositories\ClientRepositoryEloquent;
 use Illuminate\Http\Request;
 
 use CodeProject\Http\Requests;
@@ -10,31 +11,9 @@ use CodeProject\Http\Controllers\Controller;
 
 class ClientController extends Controller
 {
-    public function index()
+    public function index(ClientRepositoryEloquent $request)
     {
-    	 return \CodeProject\Models\Client::all();
+    	 return $request->all();
 	}
-
-	public function store(Request $request)
-    {
-    	 return \CodeProject\Models\Client::create($request->all());
-	}
-
-	public function show($id)
-    {
-    	 return \CodeProject\Models\Client::find($id);
-	}
-
-	public function destroy($id)
-    {
-    	 \CodeProject\Models\Client::find($id)->delete();
-	}
-
-
-    //Verificar
-    public function update (Request $request, $id)
-    {
-         return \CodeProject\Models\Client::update($request->all());
-    }
 }
    
---
+++ app/Repositories/ClientRepositoryEloquent.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marcos
+ * Date: 13/09/2016
+ * Time: 22:41
+ */
+
+namespace CodeProject\Repositories;
+
+use CodeProject\Entities\Client;
+use Prettus\Repository\Eloquent\BaseRepository;
+
+class ClientRepositoryEloquent extends BaseRepository
+{
+    public function model()
+    {
+        return Client::class;
+    }
+}
