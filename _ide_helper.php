6271f29943fa1fc709d07ee58b86f6d88c251459
marcoschags <marcoschags@hotmail.com>
2016-10-28 15:22:57 +0000

    Relacionando ProjectMembers

--- _ide_helper.php
+++ _ide_helper.php
@@ -11569,9 +11569,23 @@ namespace {
         public static function setTokenType($tokenType){
             return \LucaDegasperi\OAuth2Server\Authorizer::setTokenType($tokenType);
         }
-        
     }
-
-
 }
-
+namespace Illuminate\Support{
+    /**
+     * @method Fluent first()
+     * @method Fluent after($column)
+     * @method Fluent change()
+     * @method Fluent nullable()
+     * @method Fluent unsigned()
+     * @method Fluent unique()
+     * @method Fluent index()
+     * @method Fluent primary()
+     * @method Fluent default($value)
+     * @method Fluent onUpdate($value)
+     * @method Fluent onDelete($value)
+     * @method Fluent references($value)
+     * @method Fluent on($value)
+     */
+    class Fluent {}
+}
--- app/Entities/ProjectMember.php
+++ app/Entities/ProjectMember.php
@@ -5,7 +5,6 @@ namespace CodeProject\Entities;
 use Illuminate\Database\Eloquent\Model;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
-use RelationalExample\Model\Users;
 
 /**
  * CodeProject\Entities\Project
@@ -33,26 +32,13 @@ use RelationalExample\Model\Users;
  * @method static \Illuminate\Database\Query\Builder|\CodeProject\Entities\Project whereUpdatedAt($value)
  * @mixin \Eloquent
  */
-class Project extends Model implements Transformable
+class ProjectMember extends Model implements Transformable
 {
     use TransformableTrait;
 
     protected $fillable = [
-        'owner_id',
-        'client_id',
-        'name',
-        'description',
-        'progress',
-        'status',
-        'due_date'
+        'project_id',
+        'member_id',
     ];
 
-    public function notes()
-    {
-        return $this->hasMany(ProjectNote::class);
-    }
-    public function members()
-    {
-        return $this->belongsToMany(User::class, 'project_members', 'project_id', 'member_id');
-    }
 }
