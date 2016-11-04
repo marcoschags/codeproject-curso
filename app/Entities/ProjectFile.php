8ee68428c832ba40ce7438aaea004a472b8993ab
marcoschags <marcoschags@hotmail.com>
2016-11-04 20:26:14 +0000

    Criando ProjectFile

--- app/Entities/ProjectFile.php
+++ app/Entities/ProjectFile.php
@@ -38,25 +38,13 @@ class Project extends Model implements Transformable
     use TransformableTrait;
 
     protected $fillable = [
-        'owner_id',
-        'client_id',
         'name',
         'description',
-        'progress',
-        'status',
-        'due_date'
+        'extension',
     ];
 
-    public function notes()
+    public function project()
     {
-        return $this->hasMany(ProjectNote::class);
-    }
-    public function members()
-    {
-        return $this->belongsToMany(User::class, 'project_members', 'project_id', 'member_id');
-    }
-    public function files()
-    {
-        return $this->hasMany(ProjectFile::class);
+        return $this->belongsTo(Project::class);
     }
 }
